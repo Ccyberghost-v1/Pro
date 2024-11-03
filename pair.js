5eee43c2067a2ab9d6f0b7feb16d87ada1a0535b
Ccyberghost-v1 <malavijayadasa@gmail.com>
2024-11-03 20:36:20 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "ANJU-MD=" + string_session;
+                        let md = "GHOST-MD=" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 QUEEN ANJU MD 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/Mrrashmika/Queen_Anju-MD`; 
                         await sock.sendMessage(sock.user.id, {
@@ -83,8 +83,8 @@ text: desc,
 contextInfo: {
 externalAdReply: {
 title: "QUEEN ANJU MD",
-thumbnailUrl: "https://telegra.ph/file/adc46970456c26cad0c15.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vaj5XmgFXUubAjlU5642",
+thumbnailUrl: "https://files.catbox.moe/sx3rhh.jpg",
+sourceUrl: "https://chat.whatsapp.com/DhCdan60qzH0yoIxmybU3i",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
